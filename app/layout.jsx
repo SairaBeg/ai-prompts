fd95063dd1b7209cfc3a4357551bf1be8cdae7c7
SairaBeg <Sairabeg88@gmail.com>
2023-10-18 21:29:49 -0400

    Sign-in/out buttons
    Buttons, logo, user photo added

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import "@styles/globals.css";
+import React, { ReactNode } from "react";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+export const metadata = {
+  title: "Ai-Prompts",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
