90891b9743179511b4ee9de5b943579e83989097
SairaBeg <Sairabeg88@gmail.com>
2023-10-21 19:28:24 -0400

    Google login Auth added
    Authentication using next-auth added

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,8 +7,8 @@ import { connectToDB } from "@utils/database";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
   async session({ session }) {
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,7 +5,8 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
+
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
   useEffect(() => {
@@ -29,7 +30,7 @@ const Nav = () => {
       </Link>
       {/*Desktop Navigation*/}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -39,7 +40,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src="/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -65,10 +66,10 @@ const Nav = () => {
       </div>
       {/*Mobile Navigation*/}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/images/logo.svg"
+              src={session?.user.image}
               alt="profile"
               width={37}
               height={37}
